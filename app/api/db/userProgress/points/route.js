187c89eacceebf204454726876143b81c87d4cf4
Lorien <lorien.yuna.cho@gmail.com>
2025-01-04 12:16:35 -0500

    user points show up in navbar

--- app/api/db/userProgress/points/route.js
+++ app/api/db/userProgress/points/route.js
@@ -1,5 +1,5 @@
 import { config as dotenvConfig } from "dotenv";
-import { DynamoDBClient, UpdateItemCommand } from "@aws-sdk/client-dynamodb";
+import { DynamoDBClient, GetItemCommand, UpdateItemCommand } from "@aws-sdk/client-dynamodb";
 import { NextResponse } from 'next/server';
 
 // Load environment variables from .env file
@@ -13,12 +13,27 @@ const client = new DynamoDBClient({
     }
 });
 
-export async function PATCH(req) {
-    const tableName = "UserProgress";
+export async function GET(req) {
+    const tableName = "UserProgress"
+    try {
+        const getPoints = new GetItemCommand({
+            TableName: tableName,
+            Key: { "userId": { "S": "1" } }
+        })
 
-    const body = await req.json(); // Parse the incoming request body
-    const { userId } = body;
-    const { quantity } = body;
+        const response = await client.send(getPoints);
+        return NextResponse.json({ 'points': response.Item.points.N }, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+}
+
+export async function PATCH(req) {
+    const tableName = "UserProgress"
+
+    const body = await req.json() // Parse the incoming request body
+    const { userId } = body
+    const { quantity } = body
 
     try {
         const rewardPoints = new UpdateItemCommand({
@@ -27,11 +42,11 @@ export async function PATCH(req) {
             UpdateExpression: "ADD points :q",
             ExpressionAttributeValues: { ":q": { "N": `${quantity}` } },
             ReturnValues: "ALL_NEW"
-        });
+        })
 
         const response = await client.send(rewardPoints);
-        return NextResponse.json(response.Attributes, { status: 200 });
+        return NextResponse.json(response.Attributes, { status: 200 })
     } catch (error) {
-        return NextResponse.json({ error: error.message }, { status: 500 });
+        return NextResponse.json({ error: error.message }, { status: 500 })
     }
 }
