1906129d83d122deddf903177ded87d1de32693d
JackXuCatch22 <jackxumarch14@gmail.com>
2025-01-04 19:35:16 -0500

    Centered the avatar and layered some sample cosmetics

--- app/shopPage/page.tsx
+++ app/shopPage/page.tsx
@@ -1,24 +1,86 @@
 import ShopContainer from '../../components/shop/ShopContainer';
-import Navbar from '@/components/Navbar';
+import Image from 'next/image'
 
 const ShopPage = () => {
+    
     return (
-        <div className="min-h-screen bg-[#FFFBE8]">
-            <Navbar />
-            <div className="flex pt-16">
-                <div className="w-1/2 flex items-center justify-center">
-                    {/* Avatar or other content */}
-                    <div className="avatar-placeholder bg-gray-200 w-32 h-32 rounded-full flex items-center justify-center">
-                        <span>Avatar</span>
-                    </div>
+        <div className="flex h-screen">
+            <div className="w-1/2 flex items-center justify-center">
+                {/* Avatar or other content */}
+                {/* <div className="avatar-placeholder bg-gray-200 w-32 h-32 rounded-full flex items-center justify-center"> */}
+                    {/* <span>Avatar</span> */}
+                {/* placeholder for layering */}
+                <div 
+                id="wholeAvatar"
+                style={{ 
+                    position: "relative",
+                    width: "200px",
+                    height: "200px"
+                }}
+                >
+
+                <div 
+                id="baseAvatar"
+                style={{
+                    width: "100%",
+                    height: "100%"
+                }}
+                >
+                    <Image 
+                    src="/testvibbyBlue.png" 
+                    alt="error" 
+                    width={100} 
+                    height={100} 
+                    />
                 </div>
-                <div className="w-1/2">
-                    <ShopContainer />
+
+                <div 
+                className="hat"
+                style={{
+                    position: "absolute",
+                    top: "30%",
+                    left: "45%",
+                    transform: "translate(-50%, -50%)",
+                    width: "100%",
+                    height: "100%"
+                }}
+                >
+                    <Image 
+                        src="/testHat.png" 
+                        alt="Nothing" 
+                        width={100} 
+                        height={100}
+                    />
                 </div>
+
+                <div 
+                className="glasses"
+                style={{
+                    position: "absolute",
+                    top: "65%",
+                    left: "45%",
+                    transform: "translate(-50%, -50%)",
+                    width: "100%",
+                    height: "100%"
+                }}
+                >
+                    <Image 
+                        src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
+                        alt=""
+                        width={100} 
+                        height={100}
+                    />
+                </div>
+
+                </div>
+                {/* </div> */}
+            </div>
+            <div className="w-1/2">
+                {/* <ShopContainer /> */}
+                
             </div>
         </div>
     );
 };
 
 export default ShopPage;
-
