bbc2154ac13603a5e0c2c20a2c2eb67eac77857c
Lorien <lorien.yuna.cho@gmail.com>
2025-01-11 18:14:50 -0500

    updated inventory, points calls (buy,get, update etc.) to create entries for newly registered users as well and integrated new userid implementation

--- app/api/db/inventory/route.js
+++ app/api/db/inventory/route.js
@@ -3,6 +3,7 @@ import { DynamoDBClient, ScanCommand, GetItemCommand} from "@aws-sdk/client-dyna
 import { NextResponse } from 'next/server';
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import { cookies } from 'next/headers'
 
 // Load environment variables from .env file
 dotenvConfig();
@@ -27,23 +28,28 @@ const s3Client = new S3Client({
 // get all elements from the inventory
 export async function GET(request) {
     const origin = request.headers.get("origin");
+    
+    const userId = (await cookies()).get('userId')?.value;
 
     try {
         // get user's inventory id
         const getInventoryId = new GetItemCommand({
             TableName: "UserProgress",
-            Key: { "userId": { "S": "1" } }
+            Key: { "userId": { "S": `${userId}`} }
         })
         const userProgress = await client.send(getInventoryId)
-        const inventoryId = userProgress.Item.inventoryId
+        const inventoryId = userProgress.Item.inventoryId 
 
-        // get user's inventory
-        const getUserInventory = new GetItemCommand({
-            TableName: "UserInventory",
-            Key: { "inventoryId": inventoryId }
-        })
-        const userInventory = await client.send(getUserInventory)
-        const inventoryItems = userInventory.Item.shopItems.NS
+        let inventoryItems = []
+        if (inventoryId){
+            // get user's inventory
+            const getUserInventory = new GetItemCommand({
+                TableName: "UserInventory",
+                Key: { "inventoryId": inventoryId }
+            })
+            const userInventory = await client.send(getUserInventory)
+            inventoryItems = userInventory.Item.shopItems.NS
+        }
 
         // Scan for all shop items
         const scanCommand = new ScanCommand({
@@ -66,7 +72,7 @@ export async function GET(request) {
             item.owned = inventoryItems.includes(item.itemId.N)
             
             // Check to see if item is equipped
-            const equippedItems = userProgress.Item.currentClothes.M
+            const equippedItems = userProgress.Item.currentClothes? userProgress.currentClothes.M : []
             if (equippedItems.hasOwnProperty(item.type.S)){
                 item.equipped = equippedItems[item.type.S].N == item.itemId.N
             }
@@ -74,6 +80,7 @@ export async function GET(request) {
                 item.equipped = false;
             }
         }
+
         return NextResponse.json(response.Items, {
             headers: {
                 "Access-Control-Allow-Origin": origin,
